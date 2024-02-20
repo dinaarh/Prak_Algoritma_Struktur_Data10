076e22267037e401e22c92d469e1d1b489f85601
dinaarh <rahmawatidina0404@gmail.com>
2024-02-20 09:09:26 +0700

    P1_pertanyaan 4_10

--- p_algoritma_strukturdata10/src/P2/Buku10.java
+++ p_algoritma_strukturdata10/src/P2/Buku10.java
@@ -6,22 +6,24 @@ public class Buku10 {
     int halaman, stok, harga;
 
     void tampilInformasi() {
-        System.out.println("Judul: ", + judul);
-        System.out.println("Pengarang: ", + pengarang);
-        System.out.println("Jumlah halaman: ", + halaman);
-        System.out.println("Sisa stok: ", + stok);
-        System.out.println("Haraga: Rp", + harga);
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
     }
 
-    void terjual(int jml){
-        stok -= jml;
+    void terjual(int jml) {
+        if (stok >= jml && stok > 0) {
+            stok -= jml;
+        }
     }
 
     void restock(int jml) {
         stok += jml;
     }
 
-    void gantiHarga(int hrg){
+    void gantiHarga(int hrg) {
         harga = hrg;
     }
 }
