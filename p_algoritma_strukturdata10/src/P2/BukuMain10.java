272df6417ea51eadeb6bd9bab14daa8ab320c98d
dinaarh <rahmawatidina0404@gmail.com>
2024-02-20 13:29:47 +0700

    P2_Praktikum 1_10

--- p_algoritma_strukturdata10/src/P2/BukuMain10.java
+++ p_algoritma_strukturdata10/src/P2/BukuMain10.java
@@ -13,13 +13,25 @@ public class BukuMain10 {
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
+        System.out.println("Harga total terjual: " + bk1.hitungHargaTotal(5));
+        System.out.println("Diskon: " + bk1.hitungDiskon(bk1.hitungHargaTotal(5)));
+        System.out.println("Harga yang dibayar: "
+                + bk1.hitungHargaBayar(bk1.hitungHargaTotal(5), bk1.hitungDiskon(bk1.hitungHargaTotal(5))));
+        System.out.println();
 
         Buku10 bk2 = new Buku10("Self Reward", "Maheera Ayesha", 160, 29, 59000);
         bk2.terjual(11);
         bk2.tampilInformasi();
+        System.out.println(bk2.hitungHargaTotal(11));
+        System.out.println();
 
         Buku10 bukuDina = new Buku10("inspirasi", "dinaa", 112, 22, 56000);
         bukuDina.terjual(7);
         bukuDina.tampilInformasi();
+        System.out.println("Harga total terjual: " + bukuDina.hitungHargaTotal(7));
+        System.out.println("Diskon: " + bukuDina.hitungDiskon(bukuDina.hitungHargaTotal(7)));
+        System.out.println("Harga yang dibayar: "
+                + bukuDina.hitungHargaBayar(bukuDina.hitungHargaTotal(7),
+                        bukuDina.hitungDiskon(bukuDina.hitungHargaTotal(7))));
     }
 }
