50042309a2ea318105a353cd4cd21e1c34775f26
dinaarh <rahmawatidina0404@gmail.com>
2024-02-27 23:08:55 +0700

    P3_Percobaan2_pertanyaan 4_10

--- p_algoritma_strukturdata10/src/P3/ArrayObjects/src/main.java
+++ p_algoritma_strukturdata10/src/P3/ArrayObjects/src/main.java
@@ -4,18 +4,24 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        PersegiPanjang[] ppArray = new PersegiPanjang[3];
+        int j = 0;
         Scanner sc10 = new Scanner(System.in);
-        for (int i = 0; i < 3; i++) {
+
+        System.out.print("Masukkan banyak elemen: ");
+        j = sc10.nextInt();
+        PersegiPanjang[] ppArray = new PersegiPanjang[j];
+
+        for (int i = 0; i < j; i++) {
             ppArray[i] = new PersegiPanjang();
             System.out.println("Persegi panjang ke-" + i);
             System.out.print("Masukkan panjang: ");
             ppArray[i].panjang = sc10.nextInt();
             System.out.print("Masukkan lebar: ");
             ppArray[i].lebar = sc10.nextInt();
+
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < j; i++) {
             System.out.println("Persegi Panjang ke-" + i);
             System.out.println("Panjang:" + ppArray[i].panjang + ", lebar: " + ppArray[i].lebar);
         }
