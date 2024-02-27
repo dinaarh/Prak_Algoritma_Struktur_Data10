72d7da2ceb36c2a5c03cc413e6ed1581574f9774
dinaarh <rahmawatidina0404@gmail.com>
2024-02-28 00:31:22 +0700

    P3_Percobaan 3_pertanyaan 3_10

--- p_algoritma_strukturdata10/src/P3/ArrayObjects/src/ArraySegitiga/Segitiga.java
+++ p_algoritma_strukturdata10/src/P3/ArrayObjects/src/ArraySegitiga/Segitiga.java
@@ -1,5 +1,7 @@
 package P3.ArrayObjects.src.ArraySegitiga;
 
+import java.lang.Math;
+
 public class Segitiga {
     public int alas;
     public int tinggi;
@@ -7,5 +9,15 @@ public class Segitiga {
     public Segitiga(int a, int t) {
         alas = a;
         tinggi = t;
+    }
+
+    int hitungLuas() {
+        return (1 / 2 * alas * tinggi);
+    }
+
+    int hitungKeliling() {
+        int k = 0;
+        k = (int) Math.sqrt((alas * alas) + (tinggi * tinggi));
+        return k;
     }
 }
