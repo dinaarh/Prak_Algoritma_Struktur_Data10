f0744005f18c269c0ceb3b9265b9101275b6e2d4
dinaarh <rahmawatidina0404@gmail.com>
2024-02-27 23:54:48 +0700

    P3_percobaan 3_pertanyaan 2_10

---
+++ p_algoritma_strukturdata10/src/P3/ArrayObjects/src/ArraySegitiga/Segitiga.java
@@ -0,0 +1,11 @@
+package P3.ArrayObjects.src.ArraySegitiga;
+
+public class Segitiga {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga(int a, int t) {
+        alas = a;
+        tinggi = t;
+    }
+}
