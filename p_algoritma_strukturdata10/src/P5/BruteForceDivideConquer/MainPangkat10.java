cde0dece65f1f0a802fac979b3ef5439d18e248a
dinaarh <rahmawatidina0404@gmail.com>
2024-03-24 21:41:24 +0700

    P5_Percobaan2_10

---
+++ p_algoritma_strukturdata10/src/P5/BruteForceDivideConquer/MainPangkat10.java
@@ -0,0 +1,38 @@
+package P5.BruteForceDivideConquer;
+
+import java.util.Scanner;
+
+public class MainPangkat10 {
+    public static void main(String[] args) {
+        Scanner sc10 = new Scanner(System.in);
+
+        System.out.println("==================================");
+        System.out.print("Masukkan jumlah elemen yang dihitung: ");
+        int elemen = sc10.nextInt();
+
+        Pangkat10[] png = new Pangkat10[elemen];
+        for (int i = 0; i < elemen; i++) {
+            png[i] = new Pangkat10();
+            System.out.print("Masukkan nilai yang hendak dipangkatkan: ");
+            png[i].nilai = sc10.nextInt();
+            System.out.print("Masukkan nilai pemangkat: ");
+            png[i].pangkat = sc10.nextInt();
+        }
+
+        System.out.println("==================================");
+        System.out.println("HASIL PANGKAT - BRUTE FORCE");
+        for (int i = 0; i < elemen; i++) {
+            System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah "
+                    + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+        }
+
+        System.out.println("==================================");
+        System.out.println("HASIL PANGKAT - DIVIDE AND CONQUER");
+        for (int i = 0; i < elemen; i++) {
+            System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah "
+                    + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+        }
+
+        sc10.close();
+    }
+}
