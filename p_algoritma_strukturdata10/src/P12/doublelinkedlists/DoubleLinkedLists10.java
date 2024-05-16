8a37da6bbafc209a200f5d631fb6b1b23613fbdb
dinaarh <rahmawatidina0404@gmail.com>
2024-05-16 16:34:44 +0700

    P12 Percobaan2_10_DoubleLinkedList

--- p_algoritma_strukturdata10/src/P12/doublelinkedlists/DoubleLinkedLists10.java
+++ p_algoritma_strukturdata10/src/P12/doublelinkedlists/DoubleLinkedLists10.java
@@ -4,19 +4,19 @@ public class DoubleLinkedLists10 {
     Node10 head;
     int size;
 
-    public DoubleLinkedLists10(){
+    public DoubleLinkedLists10() {
         head = null;
         size = 0;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return head == null;
     }
 
-    public void addFirst(int item){
-        if(isEmpty()) {
+    public void addFirst(int item) {
+        if (isEmpty()) {
             head = new Node10(null, item, null);
-        }else {
+        } else {
             Node10 newNode = new Node10(null, item, head);
             head.prev = newNode;
             head = newNode;
@@ -24,12 +24,12 @@ public class DoubleLinkedLists10 {
         size++;
     }
 
-    public void addLast(int item){
-        if(isEmpty()){
+    public void addLast(int item) {
+        if (isEmpty()) {
             addFirst(item);
         } else {
             Node10 current = head;
-            while (current.next != null){
+            while (current.next != null) {
                 current = current.next;
             }
             Node10 newNode = new Node10(current, item, null);
@@ -39,7 +39,7 @@ public class DoubleLinkedLists10 {
     }
 
     public void add(int item, int index) throws Exception {
-        if(isEmpty()){
+        if (isEmpty()) {
             addFirst(item);
         } else if (index < 0 || index > size) {
             throw new Exception("Nilai indeks di luar batas");
@@ -50,7 +50,7 @@ public class DoubleLinkedLists10 {
                 current = current.next;
                 i++;
             }
-            if (current.prev == null){
+            if (current.prev == null) {
                 Node10 newNode = new Node10(null, item, current);
                 current.prev = newNode;
                 head = newNode;
@@ -65,7 +65,7 @@ public class DoubleLinkedLists10 {
         size++;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
@@ -74,16 +74,70 @@ public class DoubleLinkedLists10 {
         size = 0;
     }
 
-    public void print(){
-        if(!isEmpty()) {
+    public void print() {
+        if (!isEmpty()) {
             Node10 tmp = head;
             while (tmp != null) {
                 System.out.print(tmp.data + "\t");
                 tmp = tmp.next;
             }
             System.out.println("\nberhasil diisi");
-        } else{
+        } else {
             System.out.println("Linked Lists Kosong");
+        }
+    }
+
+    public void removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (size == 1) {
+            removeLast();
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+        Node10 current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+
+    public void remove(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas");
+        } else if (index == 0) {
+            removeFirst();
+        } else {
+            Node10 current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.next == null) {
+                current.prev.next = null;
+            } else if (current.prev == null) {
+                current = current.next;
+                current.prev = null;
+                head = current;
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+            size--;
         }
     }
 }
