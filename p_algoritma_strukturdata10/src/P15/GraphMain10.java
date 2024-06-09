11c2cb9492e5bfc4ee5fc4947a39974a4f93a93c
dinaarh <rahmawatidina0404@gmail.com>
2024-06-09 11:48:57 +0700

    P15_Percobaan2_10

--- p_algoritma_strukturdata10/src/P15/GraphMain10.java
+++ p_algoritma_strukturdata10/src/P15/GraphMain10.java
@@ -1,56 +1,69 @@
 package P15;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class GraphMain10 {
     public static void main(String[] args) throws Exception {
-        Scanner sc10 = new Scanner(System.in);
+        GraphMatriks10 gdg = new GraphMatriks10(4);
+        gdg.makeEdge(0, 1, 50);
+        gdg.makeEdge(1, 0, 60);
+        gdg.makeEdge(1, 2, 70);
+        gdg.makeEdge(2, 1, 80);
+        gdg.makeEdge(2, 3, 40);
+        gdg.makeEdge(3, 0, 90);
+        gdg.printGraph();
+        System.out.println("Hasil setelah penghapusan edge");
+        gdg.removeEdge(2, 1);
+        gdg.printGraph();
+        // Scanner sc10 = new Scanner(System.in);
         // Langkah 13 dan 14
-        Graph10 gedung = new Graph10(6);
+        // Graph10 gedung = new Graph10(6);
 
-        gedung.addEdge(0, 1, 50);
-        gedung.addEdge(0, 2, 100);
-        gedung.addEdge(1, 3, 70);
-        gedung.addEdge(2, 3, 40);
-        gedung.addEdge(3, 4, 60);
-        gedung.addEdge(4, 5, 80);
-        gedung.degree(0);
-        gedung.printGraph();
+        // gedung.addEdge(0, 1, 50);
+        // gedung.addEdge(0, 2, 100);
+        // gedung.addEdge(1, 3, 70);
+        // gedung.addEdge(2, 3, 40);
+        // gedung.addEdge(3, 4, 60);
+        // gedung.addEdge(4, 5, 80);
+        // gedung.degree(0);
+        // gedung.printGraph();
 
         // Langkah 15 dan 17
-        gedung.removeEdge(1, 3);
-        gedung.printGraph();
+        // gedung.removeEdge(1, 3);
+        // gedung.printGraph();
 
-        boolean lanjut = true;
-        System.out.println("|        Pengecekan Jalur       |");
-        System.out.println();
+        // boolean lanjut = true;
+        // System.out.println("| Pengecekan Jalur |");
+        // System.out.println();
 
-        while (lanjut) {
-            System.out.print("Masukkan gedung asal (0-5): ");
-            int asal = sc10.nextInt();
-            System.out.print("Masukkan gedung tujuan (0-5): ");
-            int tujuan = sc10.nextInt();
-            if (asal < 0 || asal > 5 || tujuan < 0 || tujuan > 5) {
-                System.out.println("Gedung yang dimasukkan tidak sesuai. Masukkan 0-5.");
-            } else {
-                if (gedung.adjacency(asal, tujuan)) {
-                    System.out.println(
-                            "Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " bertetangga");
-                } else {
-                    System.out.println(
-                            "Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " tidak bertetangga");
-                }
-            }
-            System.out.println();
+        // while (lanjut) {
+        // System.out.print("Masukkan gedung asal (0-5): ");
+        // int asal = sc10.nextInt();
+        // System.out.print("Masukkan gedung tujuan (0-5): ");
+        // int tujuan = sc10.nextInt();
+        // if (asal < 0 || asal > 5 || tujuan < 0 || tujuan > 5) {
+        // System.out.println("Gedung yang dimasukkan tidak sesuai. Masukkan 0-5.");
+        // } else {
+        // if (gedung.adjacency(asal, tujuan)) {
+        // System.out.println(
+        // "Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + "
+        // bertetangga");
+        // } else {
+        // System.out.println(
+        // "Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " tidak
+        // bertetangga");
+        // }
+        // }
+        // System.out.println();
 
-            System.out.print("Ingin melakukan pengecekan lagi? (y/n) : ");
-            char jwb = sc10.next().charAt(0);
-            if (jwb == 'n' || jwb == 'N') {
-                lanjut = false;
-            }
-            System.out.println();
+        // System.out.print("Ingin melakukan pengecekan lagi? (y/n) : ");
+        // char jwb = sc10.next().charAt(0);
+        // if (jwb == 'n' || jwb == 'N') {
+        // lanjut = false;
+        // }
+        // System.out.println();
 
-        }
-        sc10.close();
+        // }
+        // sc10.close();
     }
 }
---
+++ p_algoritma_strukturdata10/src/P15/GraphMatriks10.java
@@ -0,0 +1,32 @@
+package P15;
+
+public class GraphMatriks10 {
+    int vertex;
+    int[][] matriks;
+
+    public GraphMatriks10(int v) {
+        vertex = v;
+        matriks = new int[v][v];
+    }
+
+    public void makeEdge(int asal, int tujuan, int jarak) {
+        matriks[asal][tujuan] = jarak;
+    }
+
+    //modifikasi dari -1 jadi 0
+    public void removeEdge(int asal, int tujuan) {
+        matriks[asal][tujuan] = 0;
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < vertex; i++) {
+            System.out.print("Gedung " + (char) ('A' + i) + ": ");
+            for (int j = 0; j < vertex; j++) {
+                if (matriks[i][j] != -1) {
+                    System.err.print("Gedung " + (char) ('A' + j) + " (" + matriks[i][j] + " m), ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
