a77f147e13f89cc6f803d0016cc213b4e972dfc4
dinaarh <rahmawatidina0404@gmail.com>
2024-06-09 21:03:35 +0700

    P16_Percobaan 1_Pertanyaan 2_10

--- p_algoritma_strukturdata10/src/P16/ContohList10.java
+++ p_algoritma_strukturdata10/src/P16/ContohList10.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 public class ContohList10 {
     public static void main(String[] args) {
-        List l = new ArrayList();
+        List<Integer> l = new ArrayList<>();
         l.add(1);
         l.add(2);
         l.add(3);
-        l.add("Cireng");
+        // l.add("Cireng");
         System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n",
                 l.get(0), l.size(), l.get(l.size() - 1));
 
