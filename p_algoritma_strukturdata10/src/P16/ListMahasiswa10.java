557368bcf4b3ccae184344ff967d7ab971ca6ad1
dinaarh <rahmawatidina0404@gmail.com>
2024-06-09 23:59:21 +0700

    P16_Percobaan 3_Pertanyaan 3_10

--- p_algoritma_strukturdata10/src/P16/ListMahasiswa10.java
+++ p_algoritma_strukturdata10/src/P16/ListMahasiswa10.java
@@ -3,6 +3,7 @@ package P16;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ListMahasiswa10 {
@@ -17,9 +18,24 @@ public class ListMahasiswa10 {
         lm.tambah(m, m1, m2);
         // menampilkan list mahasiswa
         lm.tampil();
+        System.out.println();
+
+        // sorting ascending
+        System.out.println("Sorting ascending:");
+        lm.sortAscending();
+        lm.tampil();
+        System.out.println();
+
+        // sorting descending
+        System.out.println("Sorting descending:");
+        lm.sortDescending();
+        lm.tampil();
+        System.out.println();
+
         // update mahasiswa
         lm.update(lm.binarySearch("201235"), new Mahasiswa10("201235", "Akhleema Lela", "021xx2"));
         System.out.println("");
+        System.out.println("Update mahasiswa dengan Sorting ascending:");
         lm.tampil();
     }
 
@@ -42,10 +58,19 @@ public class ListMahasiswa10 {
     }
 
     int binarySearch(String nim) {
+        sortAscending();
         Mahasiswa10 key = new Mahasiswa10(nim, "", "");
         return Collections.binarySearch(mahasiswas, key);
     }
-    
+
+    public void sortAscending() {
+        Collections.sort(mahasiswas);
+    }
+
+    public void sortDescending() {
+        Collections.sort(mahasiswas, Comparator.reverseOrder());
+    }
+
     // int linearSearch(String nim) {
     // for (int i = 0; i < mahasiswas.size(); i++) {
     // if (nim.equals(mahasiswas.get(i).nim)) {
